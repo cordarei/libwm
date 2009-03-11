51b305f30c09ae66c59dca409cd6a45028a662f6
Riku Salminen <rsalmin2 at cc.hut.fi@localhost>
2009-03-12 00:52:30 +0200

    Added explicit type conversions to utf8 encoding to prevent compiler warnings

--- test/main.cpp
+++ test/main.cpp
@@ -21,25 +21,25 @@ namespace
 	{
 		if(!(codepoint & ~0x7F))
 		{
-			out[0] = codepoint & 0x7F; 
+			out[0] = unsigned char(codepoint & 0x7F);
 			return 1;
 		} else if(!(codepoint & ~0x7FF))
 		{
-			out[0] = 0xc0 | (codepoint & 0x7C0) >> 6;
-			out[1] = 0x80 | (codepoint & 0x3F);
+			out[0] = unsigned char(0xc0 | (codepoint & 0x7C0) >> 6);
+			out[1] = unsigned char(0x80 | (codepoint & 0x3F));
 			return 2;
 		} else if(!(codepoint & ~0xFFFF))
 		{
-			out[0] = 0xe0 | (codepoint & 0xF000) >> 12;
-			out[1] = 0x80 | (codepoint & 0xFC) >> 6;
-			out[2] = 0x80 | (codepoint & 0x3F);
+			out[0] = unsigned char(0xe0 | (codepoint & 0xF000) >> 12);
+			out[1] = unsigned char(0x80 | (codepoint & 0xFC) >> 6);
+			out[2] = unsigned char(0x80 | (codepoint & 0x3F));
 			return 3;
 		} else if(!(codepoint & ~0x3FFFF))
 		{
-			out[0] = 0xf0 | (codepoint & 0x1c0000) >> 18;
-			out[1] = 0x80 | (codepoint & 0x3f00) >> 12;
-			out[2] = 0x80 | (codepoint & 0xFC) >> 6;
-			out[3] = 0x80 | (codepoint & 0x3F);
+			out[0] = unsigned char(0xf0 | (codepoint & 0x1c0000) >> 18);
+			out[1] = unsigned char(0x80 | (codepoint & 0x3f00) >> 12);
+			out[2] = unsigned char(0x80 | (codepoint & 0xFC) >> 6);
+			out[3] = unsigned char(0x80 | (codepoint & 0x3F));
 			return 4;
 		}
 		
