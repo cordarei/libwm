aa13fd26d1bce245162ce46434c36c586a47dd02
Riku Salminen (rsalmin2 at cc.hut.fi) <Riku Salminen (rsalmin2 at cc.hut.fi)@localhost>
2009-03-12 00:28:50 +0200

    Test application window title bar text can be changed with keyboard

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
+#include <stdexcept>
 
 #include <wm/display.hpp>
 #include <wm/window.hpp>
@@ -13,6 +15,48 @@
 
 #include "draw.hpp"
 
+namespace
+{
+	int encode_utf8(boost::uint32_t codepoint, unsigned char *out)
+	{
+		if(!(codepoint & ~0x7F))
+		{
+			out[0] = codepoint & 0x7F; 
+			return 1;
+		} else if(!(codepoint & ~0x7FF))
+		{
+			out[0] = 0xc0 | (codepoint & 0x7C0) >> 6;
+			out[1] = 0x80 | (codepoint & 0x3F);
+			return 2;
+		} else if(!(codepoint & ~0xFFFF))
+		{
+			out[0] = 0xe0 | (codepoint & 0xF000) >> 12;
+			out[1] = 0x80 | (codepoint & 0xFC) >> 6;
+			out[2] = 0x80 | (codepoint & 0x3F);
+			return 3;
+		} else if(!(codepoint & ~0x3FFFF))
+		{
+			out[0] = 0xf0 | (codepoint & 0x1c0000) >> 18;
+			out[1] = 0x80 | (codepoint & 0x3f00) >> 12;
+			out[2] = 0x80 | (codepoint & 0xFC) >> 6;
+			out[3] = 0x80 | (codepoint & 0x3F);
+			return 4;
+		}
+		
+		throw std::runtime_error("Invalid Unicode codepoint");
+	}
+	
+	std::string encode_utf8(boost::uint32_t codepoint)
+	{
+		unsigned char buf[4];
+		int len = encode_utf8(codepoint, buf);
+		return std::string(
+			reinterpret_cast<char*>(buf+0),
+			reinterpret_cast<char*>(buf+len)
+			);
+	}
+}
+
 #if defined(WIN32)
 #define NOMINMAX // otherwise windows.h macro max will conflict with std::max
 #include <windows.h>
@@ -24,25 +68,15 @@ int main(int, char *[])
 	wm::Display display;
 	wm::Window window(display, 0, 400, 300);
 	
-	const unsigned char test_string[] = {
-		'w', 'm', ' ', 't', 'e', 's', 't', ' ', ' ',
-		'u', 't', 'f', '8', ':', ' ',
-		0xc3, 0xa4, 0xc3, 0xa4, 0x6b, 0x6b, 0xc3, 0xb6, 0x73, 0x69, 0xc3, 0xa4, 0x20,
-		0xe2, 0x82, 0xac, 0x75, 0x72, 0x6f, 0x6a, 0x61, 0
-		};
-	
-	window.setTitle(reinterpret_cast<const char*>(test_string));
-	
 	wm::Context context(window, 1, 2);
 	makeCurrent(window, context);
 
 	struct Handler : public wm::EventHandler
 	{
-		explicit Handler(wm::Window &window) : window(&window), quit_flag(false)
+		explicit Handler(wm::Window &window) : window(&window), quit_flag(false), title_string("wm test")
 		{
-			// TODO: query these from window
 			window.getSize(width, height);
-			std::cout << "window size: " << width << "x" << height << std::endl; 
+			window.setTitle(title_string.c_str());
 		}
 	
 		virtual void handle(const wm::ExposeEvent &event)
@@ -72,10 +106,18 @@ int main(int, char *[])
 		{
 			quit_flag = true;
 		}
+		
+		virtual void handle(const wm::CharacterEvent& event)
+		{
+			if(event.unicode() == '\n' || event.unicode() == '\r') title_string.clear();
+			else title_string += encode_utf8(event.unicode());
+			window->setTitle(title_string.c_str());
+		}
 
 		wm::Window *window;
-		unsigned int width, height;
 		bool quit_flag;
+		std::string title_string;
+		unsigned int width, height;
 	} handler(window);
 
 	wm::EventPrinter printer(std::cout);
