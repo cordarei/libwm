e159b708999a1d4253159b04f4d0c9473595b3a6
Riku Salminen <rsalmin2 at cc.hut.fi@localhost>
2009-07-07 13:27:18 +0300

    WGL_ARB_pixel_format support

--- src/wgl/configuration.cpp
+++ src/wgl/configuration.cpp
@@ -1,3 +1,5 @@
+#include <boost/scoped_ptr.hpp>
+
 #include <windows.h>
 
 #include <wm/display.hpp>
@@ -12,6 +14,143 @@
 
 #include <wgl/impl/extensions.hpp>
 
+namespace
+{
+	struct PixelFormatBuilder
+	{
+		virtual ~PixelFormatBuilder() { }
+
+		virtual int maxFormat(HDC hdc) const = 0;
+		virtual bool filterFormat(HDC hdc, int index) const = 0;
+		virtual wm::PixelFormat::Descriptor makeDescriptor(HDC hdc, int index) const = 0;
+	};
+
+	struct LegacyFormatBuilder : public PixelFormatBuilder
+	{
+		virtual int maxFormat(HDC hdc) const
+		{
+			int max_index = DescribePixelFormat(hdc, 1, sizeof(PIXELFORMATDESCRIPTOR), 0);
+			if(max_index == 0)
+				throw wm::Exception("DescribePixelFormat failed: " + wm::win32::getErrorMsg());
+
+			return max_index;
+		}
+
+		virtual bool filterFormat(HDC hdc, int index) const
+		{
+			PIXELFORMATDESCRIPTOR pfd;
+
+			if(!DescribePixelFormat(hdc, index, sizeof(PIXELFORMATDESCRIPTOR), &pfd))
+				throw wm::Exception("DescribePixelFormat failed: " + wm::win32::getErrorMsg());
+
+			if(!(pfd.dwFlags & PFD_SUPPORT_OPENGL) ||
+				!(pfd.dwFlags & PFD_DRAW_TO_WINDOW) ||
+				(pfd.dwFlags & PFD_NEED_PALETTE) ||
+				(pfd.dwFlags & PFD_NEED_SYSTEM_PALETTE) ||
+				!(pfd.dwFlags & PFD_DOUBLEBUFFER) ||
+				pfd.iPixelType != PFD_TYPE_RGBA)
+				return true;
+
+			return false;
+		}
+
+		virtual wm::PixelFormat::Descriptor makeDescriptor(HDC hdc, int index) const
+		{
+			PIXELFORMATDESCRIPTOR pfd;
+
+			if(!DescribePixelFormat(hdc, index, sizeof(PIXELFORMATDESCRIPTOR), &pfd))
+				throw wm::Exception("DescribePixelFormat failed: " + wm::win32::getErrorMsg());
+
+			return wm::PixelFormat::Descriptor(
+				pfd.cRedBits,
+				pfd.cBlueBits,
+				pfd.cGreenBits,
+				pfd.cAlphaBits,
+				pfd.cDepthBits,
+				pfd.cStencilBits);
+		}
+	};
+
+	struct ARB_pixel_format_Builder : public PixelFormatBuilder
+	{
+		explicit ARB_pixel_format_Builder(const wm::wgl::Extensions &extensions)
+			: extensions(extensions)
+		{
+		}
+
+
+		virtual int maxFormat(HDC hdc) const
+		{
+			const int attributes[] = { WGL_NUMBER_PIXEL_FORMATS_ARB };
+			const int num_attributes = sizeof(attributes) / sizeof(*attributes);
+			int values[sizeof(attributes) / sizeof(*attributes)];
+			if(!extensions.wglGetPixelFormatAttribivARB(hdc, 0, 0, num_attributes, attributes,	values))
+				throw wm::Exception("wglGetPixelFormatAttribiv failed: " + wm::win32::getErrorMsg());
+
+			return values[0];
+		}
+
+		virtual bool filterFormat(HDC hdc, int index) const
+		{
+			const int attributes[] = {
+				WGL_DRAW_TO_WINDOW_ARB,
+				WGL_NEED_PALETTE_ARB,
+				WGL_NEED_SYSTEM_PALETTE_ARB,
+				WGL_DOUBLE_BUFFER_ARB,
+				WGL_PIXEL_TYPE_ARB,
+				WGL_SUPPORT_OPENGL_ARB
+				};
+			const int num_attributes = sizeof(attributes) / sizeof(*attributes);
+			int values[sizeof(attributes) / sizeof(*attributes)];
+			if(!extensions.wglGetPixelFormatAttribivARB(hdc, index, 0, num_attributes, attributes,	values))
+				throw wm::Exception("wglGetPixelFormatAttribiv failed: " + wm::win32::getErrorMsg());
+
+			return
+				false 
+				|| !(values[0])								// WGL_DRAW_TO_WINDOW_ARB
+				|| (values[1])								// WGL_NEED_PALETTE_ARB
+				|| (values[2])								// WGL_NEED_SYSTEM_PALETTE_ARB
+				|| !(values[3])								// WGL_DOUBLE_BUFFER_ARB
+				|| (values[4] != WGL_TYPE_RGBA_ARB)			// WGL_PIXEL_TYPE_ARB
+				|| !(values[5])								// WGL_SUPPORT_OPENGL_ARB
+				;
+		}
+
+		virtual wm::PixelFormat::Descriptor makeDescriptor(HDC hdc, int index) const
+		{
+			const int attributes[] = {
+				WGL_RED_BITS_ARB,
+				WGL_GREEN_BITS_ARB,
+				WGL_BLUE_BITS_ARB,
+				WGL_ALPHA_BITS_ARB,
+				WGL_DEPTH_BITS_ARB,
+				WGL_STENCIL_BITS_ARB
+				};
+			const int num_attributes = sizeof(attributes) / sizeof(*attributes);
+			int values[sizeof(attributes) / sizeof(*attributes)];
+			if(!extensions.wglGetPixelFormatAttribivARB(hdc, index, 0, num_attributes, attributes,	values))
+				throw wm::Exception("wglGetPixelFormatAttribiv failed: " + wm::win32::getErrorMsg());
+
+			return wm::PixelFormat::Descriptor(
+				values[0],			// WGL_RED_BITS_ARB
+				values[1],			// WGL_GREEN_BITS_ARB
+				values[2],			// WGL_BLUE_BITS_ARB
+				values[3],			// WGL_ALPHA_BITS_ARB
+				values[4],			// WGL_DEPTH_BITS_ARB
+				values[5]			// WGL_STENCIL_BITS_ARB
+				);
+		}
+
+		const wm::wgl::Extensions &extensions;
+	};
+
+	PixelFormatBuilder* makeFormatBuilder(const wm::wgl::Extensions &extensions)
+	{
+		if(extensions.ARB_pixel_format) return new ARB_pixel_format_Builder(extensions);
+		return new LegacyFormatBuilder();
+	}
+};
+
 namespace wm
 {
 	Configuration::Configuration(Window& window)
@@ -20,57 +159,33 @@ namespace wm
 	{
 		impl->extensions.init(window.display().impl->hInstance);
 
+		boost::scoped_ptr<PixelFormatBuilder> builder(makeFormatBuilder(impl->extensions));
+
 		HWND hwnd = window.impl->hwnd;
 		HDC hdc = GetDC(hwnd);
 		if(!hdc)
 			throw wm::Exception("Can't get win32 device context" + wm::win32::getErrorMsg());
 
-		int max_index = DescribePixelFormat(hdc, 1, sizeof(PIXELFORMATDESCRIPTOR), 0);
-
-		if(max_index == 0)
+		try
 		{
-			DWORD err = GetLastError();
-			ReleaseDC(hwnd, hdc);
-			throw wm::Exception("Can't create Context" + wm::win32::getErrorMsg(err));
-		}
+			int max_index = builder->maxFormat(hdc);
 
-		for(int index = 1; index <= max_index; ++index)
-		{
-			PIXELFORMATDESCRIPTOR pfd;
-
-			if(!DescribePixelFormat(hdc, index, sizeof(PIXELFORMATDESCRIPTOR), &pfd))
+			for(int index = 1; index <= max_index; ++index)
 			{
-				DWORD err = GetLastError();
-				ReleaseDC(hwnd, hdc);
-				throw wm::Exception("Can't create Context" + wm::win32::getErrorMsg(err));
+				if(builder->filterFormat(hdc, index))
+					continue;
+
+				impl->formatdata.push_back(PixelFormat::impl_t(index));
+				impl->formats.push_back(
+					PixelFormat(
+						builder->makeDescriptor(hdc, index),
+						*this,
+						impl->formatdata.back()
+						));
 			}
-
-			if(!(pfd.dwFlags & PFD_SUPPORT_OPENGL) ||
-				!(pfd.dwFlags & PFD_DRAW_TO_WINDOW) ||
-				(pfd.dwFlags & PFD_NEED_PALETTE) ||
-				(pfd.dwFlags & PFD_NEED_SYSTEM_PALETTE) ||
-				!(pfd.dwFlags & PFD_DOUBLEBUFFER) ||
-				pfd.iPixelType != PFD_TYPE_RGBA)
-				continue;
-
-			int red, green, blue, alpha;
-			int depth, stencil;
-
-			red = pfd.cRedBits;
-			green = pfd.cGreenBits;
-			blue = pfd.cBlueBits;
-			alpha = pfd.cAlphaBits;
-
-			depth = pfd.cDepthBits;
-			stencil = pfd.cStencilBits;
-
-			impl->formatdata.push_back(PixelFormat::impl_t(index));
-			impl->formats.push_back(
-				PixelFormat(
-					PixelFormat::Descriptor(red, green, blue, alpha, depth, stencil),
-					*this,
-					impl->formatdata.back()
-					));
+		} catch(...)
+		{
+			ReleaseDC(hwnd, hdc);
 		}
 
 		ReleaseDC(hwnd, hdc);
--- src/wgl/extensions.cpp
+++ src/wgl/extensions.cpp
@@ -15,7 +15,12 @@ namespace
 	bool getProcAddress(const char *name, T *& funcptr)
 	{
 		PROC ptr = wglGetProcAddress(name);
-		if(!ptr) return false;
+		if(!ptr)
+		{
+			funcptr = 0;
+			return false;
+		}
+
 		funcptr = (T*)(ptr); //*reinterpret_cast<T**>(&ptr);
 		return true;
 	}
@@ -28,6 +33,20 @@ namespace
 			std::istream_iterator<std::string>(iss),
 			std::istream_iterator<std::string>()
 			);
+	}
+
+	bool init_WGL_ARB_pixel_format(wm::wgl::Extensions& extensions)
+	{
+		if(!extensions.supported("WGL_ARB_pixel_format"))
+			return false;
+
+		if(!getProcAddress("wglChoosePixelFormatARB", extensions.wglChoosePixelFormatARB) ||
+			!getProcAddress("wglGetPixelFormatAttribfvARB", extensions.wglGetPixelFormatAttribfvARB) ||
+			!getProcAddress("wglGetPixelFormatAttribivARB", extensions.wglGetPixelFormatAttribivARB))
+			throw wm::Exception("Can't initialize WGL_ARB_pixel_format");
+
+		extensions.ARB_pixel_format = true;
+		return true;
 	}
 }
 
@@ -53,6 +72,8 @@ namespace wm
 			ReleaseDC(dummywnd.hwnd, hdc);
 
 			make_set(extensions, extstring);
+
+			init_WGL_ARB_pixel_format(*this);
 		}
 
 		bool Extensions::supported(const std::string &name) const
