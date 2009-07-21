c8d38f4008a30d28f2a83983662d419f4bf85219
Riku Salminen <rsalmin2 at cc.hut.fi@localhost>
2009-07-21 16:16:20 +0300

    Added config.hpp to solve building Win32 static libs and __declspec(dllexport) in export.hpp

--- include/wm/export.hpp
+++ include/wm/export.hpp
@@ -1,5 +1,7 @@
+#include <wm/config.hpp>
+
 #ifndef WM_EXPORT
-	#ifdef WIN32
+	#if defined(WIN32) && defined(WM_BUILD_SHARED_LIBS)
 		#ifdef wm_EXPORTS
 			// wm_EXPORTS is defined by CMake when building DLL's
 			#define WM_EXPORT __declspec(dllexport)
